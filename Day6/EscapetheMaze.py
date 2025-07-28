d1424953b9ade38cc422b37855afc7d78d2b7970
Swaran Noble <ysnoble07@gmail.com>
2025-07-28 12:11:55 +0530

    Day6 Python

---
+++ Day6/EscapetheMaze.py
@@ -0,0 +1,23 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+#Please follow the above link and use the code below to solve the maze problem
+
+# These functions are pre-defined in the link above.
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()        
