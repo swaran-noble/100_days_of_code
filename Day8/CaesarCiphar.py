1c0eb9900d8d7a69b04fa1064628f7c6170b5dd5
Swaran Noble <ysnoble07@gmail.com>
2025-07-30 23:19:05 +0530

    Day8 Python

---
+++ Day8/CaesarCiphar.py
@@ -0,0 +1,27 @@
+from art import logo
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text,shift_amount,encode_or_decode):
+    output_text=""
+
+    if encode_or_decode == "decode":
+        shift_amount*=-1
+
+    for letter in original_text:
+        if letter in alphabet:
+            new_position = (alphabet.index(letter) + shift_amount) % len(alphabet)
+            output_text+=alphabet[new_position]
+        else:
+            output_text+=letter
+    print(f"Here is the {encode_or_decode}d result: {output_text}")                
+
+game_continue = True
+while game_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    response=input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    if response=="no":
+        game_continue=False
