f896c1643850fa9d4aca11a55bbd5e9af1729f95
Swaran Noble <ysnoble07@gmail.com>
2025-09-21 00:07:00 +0530

    Day52 Python

--- Day52/main.py
+++ Day52/main.py
@@ -1,47 +1,83 @@
-import os
-from bs4 import BeautifulSoup
-import requests
-import smtplib
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementClickInterceptedException
+import time
 
-# Practice
-PRACTICE_URL = "https://appbrewery.github.io/instant_pot/"
-# Live Site
-LIVE_URL = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
-header={
-    "Accept-Language":"en-GB,en-US,en;q=0.9",
-    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-}
-response = requests.get(PRACTICE_URL,headers=header)
+SIMILAR_ACCOUNT = "fukkard"
+USERNAME = "catch_a.break"
+PASSWORD = "Chinna@617"
 
-soup = BeautifulSoup(response.content,"html.parser")
-#print(soup.prettify())
+class InstaFollower():
 
-price = soup.find(class_="a-offscreen").get_text()
-#print(price)
+    def __init__(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("detach",True)
+        self.driver = webdriver.Chrome(options=chrome_options)
 
-price_without_currency = price.split("$")[1]
+    def login(self):
+        url = "https://www.instagram.com/accounts/login/"
+        self.driver.get(url)
+        time.sleep(4.2)
 
-price_as_float = float(price_without_currency)
-#print(price_as_float)
+        # Check if the cookie warning is present on the page
+        decline_cookies_xpath = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[2]"
+        cookie_warning = self.driver.find_elements(By.XPATH, decline_cookies_xpath)
+        if cookie_warning:
+            # Dismiss the cookie warning by clicking an element or button
+            cookie_warning[0].click()
 
-# ========== Send an Email =========
+        username = self.driver.find_element(by=By.NAME, value="username")
+        password = self.driver.find_element(by=By.NAME, value="password")
 
-title = soup.find(id="productTitle").get_text().strip()
-print(title)
+        username.send_keys(USERNAME)
+        password.send_keys(PASSWORD)
 
-BUY_PRICE = 100
+        time.sleep(2.1)
+        password.send_keys(Keys.ENTER)
 
-if price_as_float < BUY_PRICE:
-    message = f"{title} is on sale for {price}!"
+        time.sleep(4.3)
+        # Click "Not now" and ignore Save-login info prompt
+        save_login_prompt = self.driver.find_element(by=By.XPATH, value="//div[contains(text(), 'Not now')]")
+        if save_login_prompt:
+            save_login_prompt.click()
 
-    with smtplib.SMTP("smtp.gmail.com",587) as connection:
-        connection.starttls()
-        result = connection.login(os.environ["MY_EMAIL"],os.environ["MY_PASSWORD"])
-        connection.sendmail(
-            from_addr=os.environ["MY_EMAIL"],
-            to_addrs=os.environ["MY_EMAIL"],
-            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{PRACTICE_URL}".encode("utf-8")
-        )
+        time.sleep(3.7)
+        # Click "not now" on notifications prompt
+        notifications_prompt = self.driver.find_element(by=By.XPATH, value="// button[contains(text(), 'Not Now')]")
+        if notifications_prompt:
+            notifications_prompt.click()
 
+    def find_followers(self):
+        time.sleep(5)
+        # Show followers of the selected account.
+        self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}/followers")
+        time.sleep(5.2)
+        # The xpath of the modal that shows the followers will change over time. Update yours accordingly.
+        modal_xpath = "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]"
+        modal = self.driver.find_element(by=By.XPATH, value=modal_xpath)
+        for i in range(10):
+            # In this case we're executing some Javascript, that's what the execute_script() method does.
+            # The method can accept the script as well as an HTML element.
+            # The modal in this case, becomes the arguments[0] in the script.
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            time.sleep(2)
 
+    def follow(self):
+        # Check and update the (CSS) Selector for the "Follow" buttons as required.
+        all_buttons = self.driver.find_elements(By.CSS_SELECTOR, value='._aaco button')
+
+        for button in all_buttons:
+            try:
+                button.click()
+                time.sleep(1.1)
+            # Clicking button for someone who is already being followed will trigger dialog to Unfollow/Cancel
+            except ElementClickInterceptedException:
+                cancel_button = self.driver.find_element(by=By.XPATH, value="//button[contains(text(), 'Cancel')]")
+                cancel_button.click()
+
+bot = InstaFollower()
+bot.login()
+bot.find_followers()
+bot.follow()
 
