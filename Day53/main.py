9ea59a7d900740e8bb15f4e66e9c569c2d359639
Swaran Noble <ysnoble07@gmail.com>
2025-09-20 22:55:48 +0530

    Day53 Python

--- Day53/main.py
+++ Day53/main.py
@@ -1,47 +1,68 @@
-import os
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 import requests
-import smtplib
 
-# Practice
-PRACTICE_URL = "https://appbrewery.github.io/instant_pot/"
-# Live Site
-LIVE_URL = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
-header={
-    "Accept-Language":"en-GB,en-US,en;q=0.9",
-    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+# Part 1 - Scrape the links, addresses, and prices of the rental properties
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
 }
-response = requests.get(PRACTICE_URL,headers=header)
 
-soup = BeautifulSoup(response.content,"html.parser")
-#print(soup.prettify())
+zillow_url = "https://appbrewery.github.io/Zillow-Clone/"
+response = requests.get(zillow_url,headers=header)
+data = response.text
+soup = BeautifulSoup(data,"html.parser")
 
-price = soup.find(class_="a-offscreen").get_text()
-#print(price)
+# Create a list of all the links on the page using a CSS Selector
+all_link_elements = soup.select(".StyledPropertyCardDataWrapper a")
 
-price_without_currency = price.split("$")[1]
+all_links = [link["href"] for link in all_link_elements]
+print(f"There are {len(all_links)} links to individual listings in total: \n")
+print(all_links)
 
-price_as_float = float(price_without_currency)
-#print(price_as_float)
+all_address_elements = soup.select(".StyledPropertyCardDataWrapper address")
+all_addresses = [address.get_text().replace(" | ", " ").strip() for address in all_address_elements]
+print(f"\n After having been cleaned up, the {len(all_addresses)} addresses now look like this: \n")
+print(all_addresses)
 
-# ========== Send an Email =========
+# Create a list of all the prices on the page using a CSS Selector
+# Get a clean dollar price and strip off any "+" symbols and "per month" /mo abbreviation
+all_price_elements = soup.select(".PropertyCardWrapper span")
+all_prices = [price.get_text().replace("/mo", "").split("+")[0] for price in all_price_elements if "$" in price.text]
+print(f"\n After having been cleaned up, the {len(all_prices)} prices now look like this: \n")
+print(all_prices)
 
-title = soup.find(id="productTitle").get_text().strip()
-print(title)
+# Optional - Keep the browser open (helps diagnose issues if the script crashes)
+google_forms = "https://forms.gle/9WBTMpMX2uzgeSjTA"
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
 
-BUY_PRICE = 100
+for n in range(len(all_links)):
+    # TODO: Add fill in the link to your own Google From
+    driver.get(google_forms)
+    time.sleep(2)
 
-if price_as_float < BUY_PRICE:
-    message = f"{title} is on sale for {price}!"
+    # Use the xpath to select the "short answer" fields in your Google Form.
+    # Note, your xpath might be different if you created a different form.
+    address = driver.find_element(by=By.XPATH,
+                                  value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(by=By.XPATH,
+                                value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element(by=By.XPATH,
+                               value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit_button = driver.find_element(by=By.XPATH,
+                                        value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
 
-    with smtplib.SMTP("smtp.gmail.com",587) as connection:
-        connection.starttls()
-        result = connection.login(os.environ["MY_EMAIL"],os.environ["MY_PASSWORD"])
-        connection.sendmail(
-            from_addr=os.environ["MY_EMAIL"],
-            to_addrs=os.environ["MY_EMAIL"],
-            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{PRACTICE_URL}".encode("utf-8")
-        )
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    submit_button.click()
+
+
 
 
 
