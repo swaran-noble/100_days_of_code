a274a0e045b6c8c9242e1afdc3562e04485c7aea
Swaran Noble <ysnoble07@gmail.com>
2025-09-28 12:02:56 +0530

    Day59 Python

---
+++ Day59/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+posts = requests.get("https://api.npoint.io/e11bc85c05b8c4de7271").json()
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html",all_posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html",post=requested_post)
+
+if __name__ == "__main__":
+    app.run(debug=True)
